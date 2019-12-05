957a33c0a1cb94067176381a676ad19dbf9ae4bf
comentor <loghire@gmail.com>
2019-12-04 22:10:26 -0500

    chore: specify port

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ io.sockets.on('connection', function(socket) {
     });
 
 });
-
-const server = http.listen(8080, function() {
-    console.log('listening on *:8080');
+const port = process.env.PORT || 8080;;
+const server = http.listen(port, function() {
+    console.log('listening on *:' + port);
 });
